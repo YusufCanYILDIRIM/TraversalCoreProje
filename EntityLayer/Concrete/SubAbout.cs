9735288ce47e1b1da3a90824b9a2674db0e047b6
Yusuf Can YILDIRIM <yusufyildirimcan@outlook.com>
2024-10-15 13:20:45 +0300

    Add project files.

---
+++ EntityLayer/Concrete/SubAbout.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityLayer.Concrete
+{
+    public class SubAbout
+    {
+        //bu key benim id'lerimi birincil anahtar yapıcak
+        [Key]
+        public int SubAboutID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
